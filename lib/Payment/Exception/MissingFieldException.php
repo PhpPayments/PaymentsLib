4827a5958468b5546a889ccb4e0b99127dcd3525
Florian Krämer <florian.kraemer@cakedc.com>
2013-02-18 21:03:18 +0100

    Adding a MissingFieldException

---
+++ lib/Payment/Exception/MissingFieldException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Payment\Exception;
+/**
+ * PaymentApiException
+ *
+ * @author Florian Krämer
+ * @copyright 2013 Florian Krämer
+ * @license MIT
+ */
+class MissingFieldException extends \Payment\Exception\PaymentException {
+
+}
