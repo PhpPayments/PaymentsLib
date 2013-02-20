fa9a97f3e1e191e1d1921793bebdfe48aaa14b20
Florian Krämer <florian.kraemer@cakedc.com>
2013-02-20 22:27:55 +0100

    Adding MissingConfigException.php

---
+++ lib/Payment/Exception/MissingConfigException.php
@@ -0,0 +1,12 @@
+<?php
+namespace \Payment\Exception;
+/**
+ * MissingConfigException
+ *
+ * @author Florian Krämer
+ * @copyright 2013 Florian Krämer
+ * @license MIT
+ */
+class MissingConfigException extends \Payment\Exception\PaymentException {
+
+}
